026ede0c8496c9fbfb4c5664399186378d8d4fd2
wangdongyang <wangdongyang@guohuaigroup.com>
2018-09-07 17:50:11 +0800

    Merge branch 'master' of https://github.com/w522824105/eureka-cbapter1
    # Conflicts:
    #	README.md

--- eureka-server2/src/main/java/com/my/EurekaServer2Application.java
+++ eureka-server2/src/main/java/com/my/EurekaServer2Application.java
@@ -10,9 +10,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaServerApplication {
+public class EurekaServer2Application {
 
     public static void main(String[] args) {
-        SpringApplication.run( EurekaServerApplication.class, args );
+        SpringApplication.run( EurekaServer2Application.class, args );
     }
 }
